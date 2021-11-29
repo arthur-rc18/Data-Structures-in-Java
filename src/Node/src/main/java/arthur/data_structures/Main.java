7b7d1b76b4b7a1aef1016d89d60529b4423a953b
Arthur <arthurrodri015@gmail.com>
2021-11-29 20:03:41 -0300

    Changing the main

--- src/Node/src/main/java/arthur/data_structures/Main.java
+++ src/Node/src/main/java/arthur/data_structures/Main.java
@@ -2,12 +2,12 @@ package arthur.data_structures;
 
 public class Main {
     public static void main(String[] args) {
-        Node n1 = new Node("n1 content");
-        Node n2 = new Node("n2 content");
+        Node<String> n1 = new Node<>("n1 content");
+        Node<String> n2 = new Node<>("n2 content");
 
         n1.setNextNode(n2);
 
-        Node n3 = new Node("n3 content");
+        Node<String> n3 = new Node<>("n3 content");
         n2.setNextNode(n3);
 
         System.out.println(n1);
