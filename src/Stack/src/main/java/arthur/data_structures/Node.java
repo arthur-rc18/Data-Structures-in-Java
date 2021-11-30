2ed2ed7d7ae082069dc53f118e7dad13d1e103dd
Arthur <arthurrodri015@gmail.com>
2021-11-30 20:19:12 -0300

    Finalizing the Node class

--- src/Stack/src/main/java/arthur/data_structures/Node.java
+++ src/Stack/src/main/java/arthur/data_structures/Node.java
@@ -4,5 +4,30 @@ public class Node {
     private int data;
     private Node nextNode = null;
 
-    
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "data=" + data +
+                '}';
+    }
 }
